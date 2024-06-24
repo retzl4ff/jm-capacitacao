b196ab0b59dd30f27b5ec2ce1e4e7209abd4ab91
Jonata Carlins <128560438+JonataCarlins@users.noreply.github.com>
2024-06-24 21:53:16 +0000

    commit Jon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Footer from './footer';
 import Home from './home';
 import NavBar from './navbar';
+import Servicos from './Servicos';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Home />
 
 
-
+      <Servicos />
       <Footer />
     </div>
   );
